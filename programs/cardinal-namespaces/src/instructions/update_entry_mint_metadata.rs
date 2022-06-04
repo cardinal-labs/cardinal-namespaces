7dc4b693c2bde03ee1b427bd904feeb8ff8f89a5
Chewing Glass <chewinglass123@gmail.com>
2022-06-04 15:09:29 -0500

    Add ability to update metadata royalties

--- programs/cardinal-namespaces/src/instructions/update_entry_mint_metadata.rs
+++ programs/cardinal-namespaces/src/instructions/update_entry_mint_metadata.rs
@@ -1,4 +1,4 @@
-use metaplex_token_metadata::{instruction::update_metadata_accounts, state::Data};
+use metaplex_token_metadata::{instruction::update_metadata_accounts, state::{Data, Creator as MCreator}};
 use {
     crate::{errors::*, state::*},
     anchor_lang::{prelude::*, solana_program::program::invoke_signed},
@@ -9,12 +9,30 @@ pub struct UpdateEntryMintMetadataCtx<'info> {
     namespace: Box<Account<'info, Namespace>>,
     #[account(constraint = entry.namespace == namespace.key() @ ErrorCode::InvalidNamespace)]
     entry: Box<Account<'info, Entry>>,
+    #[account(constraint = approve_authority.key() == namespace.approve_authority.unwrap() @ ErrorCode::InvalidApproveAuthority)]
+    approve_authority: Signer<'info>,
     #[account(mut)]
     certificate_mint_metadata: UncheckedAccount<'info>,
     token_metadata_program: UncheckedAccount<'info>,
 }
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
+pub struct Creator {
+    pub address: Pubkey,
+    pub verified: bool,
+    // In percentages, NOT basis points ;) Watch out!
+    pub share: u8,
+}
 
-pub fn handler(ctx: Context<UpdateEntryMintMetadataCtx>) -> ProgramResult {
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
+pub struct UpdateMetadataArgs {
+    /// Royalty basis points that goes to creators in secondary sales (0-10000)
+    pub seller_fee_basis_points: u16,
+    /// Array of creators, optional
+    pub creators: Option<Vec<Creator>>,
+    pub primary_sale_happened: Option<bool>
+}
+
+pub fn handler(ctx: Context<UpdateEntryMintMetadataCtx>, args: UpdateMetadataArgs) -> ProgramResult {
     let namespace_seeds = &[NAMESPACE_PREFIX.as_bytes(), ctx.accounts.namespace.name.as_bytes(), &[ctx.accounts.namespace.bump]];
     let namespace_signer = &[&namespace_seeds[..]];
 
@@ -39,10 +57,14 @@ pub fn handler(ctx: Context<UpdateEntryMintMetadataCtx>) -> ProgramResult {
                 name: ctx.accounts.entry.name.clone() + "." + &ctx.accounts.namespace.name.to_string(),
                 symbol: "NAME".to_string(),
                 uri: "https://api.cardinal.so/metadata/".to_string() + &ctx.accounts.entry.mint.to_string(),
-                creators: None,
-                seller_fee_basis_points: 0,
+                creators: args.creators.map(|creators| creators.iter().map(|c| MCreator {
+                  address: c.address,
+                  verified: c.verified,
+                  share: c.share
+                }).collect()),
+                seller_fee_basis_points: args.seller_fee_basis_points,
             }),
-            None,
+            args.primary_sale_happened,
         ),
         &[ctx.accounts.certificate_mint_metadata.to_account_info(), ctx.accounts.namespace.to_account_info()],
         namespace_signer,
--- programs/cardinal-namespaces/src/lib.rs
+++ programs/cardinal-namespaces/src/lib.rs
@@ -91,7 +91,7 @@ pub mod namespaces {
         update_claim_request::handler(ctx, is_approved)
     }
 
-    pub fn update_entry_mint_metadata(ctx: Context<UpdateEntryMintMetadataCtx>) -> ProgramResult {
-        update_entry_mint_metadata::handler(ctx)
+    pub fn update_entry_mint_metadata(ctx: Context<UpdateEntryMintMetadataCtx>, args: UpdateMetadataArgs) -> ProgramResult {
+        update_entry_mint_metadata::handler(ctx, args)
     }
 }
