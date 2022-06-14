40d412dd98d6bd6f68cbf74c89a457d7b10a6f95
jpbogle <jpbogle22@gmail.com>
2022-06-13 23:33:06 -0400

    Counter fix

--- programs/cardinal-namespaces/src/instructions/requests/update_claim_request.rs
+++ programs/cardinal-namespaces/src/instructions/requests/update_claim_request.rs
@@ -1,11 +1,12 @@
 use {
-    crate::{errors::ErrorCode, state::*},
+    crate::{errors::ErrorCode, instructions::assert_derivation, state::*},
     anchor_lang::prelude::*,
 };
 
 #[derive(Accounts)]
 pub struct UpdateClaimRequestCtx<'info> {
-    name_entry: Account<'info, Entry>,
+    /// CHECK: This is not dangerous because we check inside the handler
+    name_entry: UncheckedAccount<'info>,
     namespace: Account<'info, Namespace>,
     #[account(constraint = approve_authority.key() == namespace.approve_authority.unwrap() @ ErrorCode::InvalidApproveAuthority)]
     approve_authority: Signer<'info>,
@@ -16,6 +17,17 @@ pub struct UpdateClaimRequestCtx<'info> {
 pub fn handler(ctx: Context<UpdateClaimRequestCtx>, is_approved: bool) -> Result<()> {
     let rent_request = &mut ctx.accounts.rent_request;
     rent_request.is_approved = is_approved;
-    rent_request.counter = ctx.accounts.name_entry.claim_request_counter;
+    rent_request.counter = 0;
+
+    assert_derivation(
+        &ctx.program_id,
+        &ctx.accounts.name_entry.to_account_info(),
+        &[ENTRY_SEED.as_bytes(), ctx.accounts.namespace.key().as_ref(), rent_request.entry_name.as_bytes()],
+    )?;
+
+    if !ctx.accounts.name_entry.data_is_empty() {
+        let name_entry = Account::<Entry>::try_from(&ctx.accounts.name_entry)?;
+        rent_request.counter = name_entry.claim_request_counter;
+    }
     Ok(())
 }
